6193b7bb8aa236252608e6ae69448ca2ace44e72
António Abranches Pinto <antoniomariabranches@gmail.com>
2022-05-27 17:27:30 +0100

    Update dollar.c

--- src/token/dollar.c
+++ src/token/dollar.c
@@ -6,7 +6,7 @@
 /*   By: amaria-m <amaria-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 11:45:31 by amaria-m          #+#    #+#             */
-/*   Updated: 2022/05/27 17:09:22 by amaria-m         ###   ########.fr       */
+/*   Updated: 2022/05/27 17:26:18 by amaria-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,20 @@
 
 int ft_read_dollar(char *str, int index)
 {
-    char    *quote;
-    int s_quote;
-    int d_quote;
-
-    s_quote = 0;
-    d_quote = 0;
-    quote = string().strnstr(str, "\'", string().size(str));
-    if (quote)
-        s_quote = (string().strnstr(str, "\'", string().size(str)) - str);
-    quote = string().strnstr(str, "\"", string().size(str));
-    if (quote)
-        d_quote = (string().strnstr(str, "\"", string().size(str)) - str);
-    return(str);
-    
-    
+	
 }
 
 char    *ft_dollar(char *str)
 {
-    char    *cpy;
-    char    *mem;
+	char    *cpy;
+	char    *mem;
 
-    mem = string().strnstr(str, "$", string().size(str));
-    if (mem)
-    {
-        cpy = string().copy_n(str, mem - str);
-        //printf("%s\n", cpy);
-        free(cpy);
-    }
-    return (str);
+	mem = string().strnstr(str, "$", string().size(str));
+	if (mem)
+	{
+		cpy = string().copy_n(str, mem - str);
+		//printf("%s\n", cpy);
+		free(cpy);
+	}
+	return (str);
 }
