3e346a0674602bbcd24392d9f4b084f18d3a2288
Ezequiel <55319859+zico15@users.noreply.github.com>
2022-05-27 17:26:23 +0100

    norminette string_util

--- headers/string_util.h
+++ headers/string_util.h
@@ -6,7 +6,7 @@
 /*   By: edos-san <edos-san@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 20:38:14 by edos-san          #+#    #+#             */
-/*   Updated: 2022/05/27 17:22:13 by edos-san         ###   ########.fr       */
+/*   Updated: 2022/05/27 17:25:56 by edos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 
 # include <stdlib.h>
 
-char			*__join(const char *str1, const char *str2);
-char			**__split(char const *s, char c, int j, char **list);
-char			*__copy_n(const char *str, int n);
-char			*__str_trim(const char *str);
-int				__equals(const char *str1, const char *str2);
-int				__equals_n(const char *str1, const char *str2, int n);
-int				__is_space(char c);
-char			*__strnstr(const char *haystack, const char *needle, size_t len);
+char		*__join(const char *str1, const char *str2);
+char		**__split(char const *s, char c, int j, char **list);
+char		*__copy_n(const char *str, int n);
+char		*__str_trim(const char *str);
+int			__equals(const char *str1, const char *str2);
+int			__equals_n(const char *str1, const char *str2, int n);
+int			__is_space(char c);
+char		*__strnstr(const char *haystack, const char *needle, size_t len);
 
 #endif
