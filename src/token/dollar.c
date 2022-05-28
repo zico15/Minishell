3c7adc023ccbfc20f99706a5d933aee8263deb8f
Ezequiel <55319859+zico15@users.noreply.github.com>
2022-05-28 17:37:30 +0100

    função estava incompleta
    estava dando erro pra testar o programa

--- src/token/dollar.c
+++ src/token/dollar.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   dollar.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amaria-m <amaria-m@student.42.fr>          +#+  +:+       +#+        */
+/*   By: edos-san <edos-san@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 11:45:31 by amaria-m          #+#    #+#             */
-/*   Updated: 2022/05/27 17:26:18 by amaria-m         ###   ########.fr       */
+/*   Updated: 2022/05/28 17:36:00 by edos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 int ft_read_dollar(char *str, int index)
 {
-	
+	(void) str;
+	(void) index;
+	return (0);
 }
 
 char    *ft_dollar(char *str)
