1b4cde7f85b9bbbd07e82005dee5b3de2ad9b11b
António Abranches Pinto <antoniomariabranches@gmail.com>
2022-05-28 20:55:42 +0100

    dsa

--- src/token/dollar.c
+++ src/token/dollar.c
@@ -3,31 +3,67 @@
 /*                                                        :::      ::::::::   */
 /*   dollar.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amaria-m <amaria-m@student.42.fr>          +#+  +:+       +#+        */
+/*   By: amaria-m <amaria-m@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 11:45:31 by amaria-m          #+#    #+#             */
-/*   Updated: 2022/05/27 17:26:18 by amaria-m         ###   ########.fr       */
+/*   Updated: 2022/05/27 20:36:58 by amaria-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_token.h>
 
+int	*ft_count_quotes(char *str, int index, int *arr)
+{
+	int	i;
+
+	i = 0;
+	while (i < 4)
+		arr[i++] = 0;
+	i = index;
+	while (str[--i] && i >= 0)
+	{
+		arr[0] += (str[i] == '\"');
+		arr[1] += (str[i] == '\'');
+	}
+	i = index;
+	while (str[++i])
+	{
+		arr[2] += (str[i] == '\'');
+		arr[3] += (str[i] == '\"');
+	}
+	return (arr);
+}
+
 int ft_read_dollar(char *str, int index)
 {
-	
+	int	i;
+	int	*arr;
+
+	arr = malloc(sizeof(int) * 4);
+	arr = ft_count_quotes(str, index, arr);
+	i = -1;
+	while (++i < 4)
+		printf("arr %i: %i\n", i, arr[i]);
+	free(arr);
+	return (0);
 }
 
 char    *ft_dollar(char *str)
 {
-	char    *cpy;
-	char    *mem;
+	int	i;
 
-	mem = string().strnstr(str, "$", string().size(str));
-	if (mem)
+	i = 0;
+	while (str && str[i])
 	{
-		cpy = string().copy_n(str, mem - str);
-		//printf("%s\n", cpy);
-		free(cpy);
+		if (str[i] == '$')
+		{
+			if (!ft_read_dollar(str, i))
+				break;
+		}
+		i++;
 	}
 	return (str);
 }
+
+// echo "$TERM"
+// echo 
