054f46d7faec4e749740ede70e0196d1a509576f
António Abranches Pinto <antoniomariabranches@gmail.com>
2022-05-30 17:16:31 +0100

    dealing with quotes

--- src/token/token.c
+++ src/token/token.c
@@ -3,14 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   token.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amaria-m <amaria-m@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: amaria-m <amaria-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 16:40:58 by amaria-m          #+#    #+#             */
-/*   Updated: 2022/05/27 11:51:27 by amaria-m         ###   ########.fr       */
+/*   Updated: 2022/05/30 17:14:31 by amaria-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_token.h>
+
+static int	ft_move_j(char *line, int i, int j, int *separator)
+{
+	j = i;
+	while (line[j] && (ft_quotes(&line[j]) || *separator \
+	|| ft_separator(line[j])))
+	{
+		if (ft_separator(line[j++]))
+			*separator = 0;
+	}
+	return (j);
+}
 
 char	**token(char *line)
 {
@@ -26,19 +38,17 @@ char	**token(char *line)
 	tokens = new_array();
 	i = 0;
 	separator = 0;
-	line = ft_dollar(line);
 	while (line[i])
 	{
-		j = i;
-		while (line[j] && (ft_quotes(&line[j]) || separator || ft_separator(line[j])))
-			if (ft_separator(line[j++]))
-				separator = 0;
+		j = ft_move_j(line, i, j, &separator);
 		if (j > i)
 		{
 			separator = 1;
-			array(tokens)->add(string().copy_n(line + i + (line[i] == '|'), j - i - (line[i] == '|')));
+			(array(tokens))->add(string().copy_n(line + i + \
+			(line[i] == '|'), j - i - (line[i] == '|')));
 		}
 		i = j;
 	}
+	ft_handle_quotes(tokens);
 	return (ft_lst_to_arr(tokens));
 }
