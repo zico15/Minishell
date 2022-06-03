0983311f0eceb0d2761ece8bfd023fbcc7a053fa
Ezequiel <55319859+zico15@users.noreply.github.com>
2022-06-03 12:59:15 +0100

    Update teste3.c

--- teste3.c
+++ teste3.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*else
+		waitpid(g_sig.pid, &ret, 0);
+	if (g_sig.sigint == 1 || g_sig.sigquit == 1)
+		return (g_sig.exit_status);
+	ret = (ret == 32256 || ret == 32512) ? ret / 256 : !!ret;*/
+
 int main(void)
 {
 	int i = 0;
